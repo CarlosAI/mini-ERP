cf4ab3680cc6afaa350cc9596f5e06ae69e6da9f
unknown <ricki1998@outlook.com>
2019-03-07 12:20:52 -0600

    Diseño formularios

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :productos
   resources :contactos
-  resources :terceros
+  resources :terceros 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
